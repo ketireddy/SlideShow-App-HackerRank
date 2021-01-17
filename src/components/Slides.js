56f2c8a9ee3d236c6e16378d377688e4d4ff1293
ketiv <Vinesh.Ketireddy@pega.com>
2021-01-17 20:28:45 +0530

    hackerrank test SLIDESHOW

---
+++ src/components/Slides.js
@@ -0,0 +1,33 @@
+import React,{useState,useEffect} from 'react';
+
+function Slides({slides}) {
+const [count,setCount] = useState(0);
+
+    return (
+        <div>
+            <div id="navigation" className="text-center">
+                <button 
+                    data-testid="button-restart" 
+                    className="small outlined" 
+                    onClick={() => setCount(0)} 
+                    disabled={count===0 ? true:false}>Restart</button>
+                <button data-testid="button-prev" 
+                    className="small" 
+                    onClick={() => setCount(count-1)} 
+                    disabled={count===0 ? true:false}>Prev</button>
+                <button 
+                    data-testid="button-next" 
+                    className="small"
+                    onClick={() => setCount(count+1)}
+                    disabled={count===slides.length-1}>Next</button>
+            </div>
+            <div id="slide" className="card text-center">
+                <h1 data-testid="title">{slides[count].title}</h1>
+                <p data-testid="text">{slides[count].text}</p>
+            </div>
+        </div>
+    );
+
+}
+
+export default Slides;
